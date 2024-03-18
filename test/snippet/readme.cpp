d5ce6e6c26681042b78ec6b665d4fa23c74fd1bc
Enrico Seiler <enrico.seiler@hotmail.de>
2024-03-18 11:40:10 +0100

    WIP

--- test/snippet/readme.cpp
+++ test/snippet/readme.cpp
@@ -1,94 +1,86 @@
-// SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
-// SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
-// SPDX-License-Identifier: CC0-1.0
+#include <hibf/interleaved_bloom_filter.hpp>
 
-#include <cinttypes>  // for uint64_t, int64_t
-#include <cstddef>    // for size_t
-#include <functional> // for function
-#include <iostream>   // for operator<<, basic_ostream, cout
-#include <ranges>     // for __fn, iota, views
-#include <vector>     // for vector
+// Shared parameters
+uint32_t const num_bins{32u};
+uint8_t const num_threads{8u};
+uint8_t const num_hashes{3u};
 
-#include <hibf/config.hpp>                                // for insert_iterator, config
-#include <hibf/hierarchical_interleaved_bloom_filter.hpp> // for hierarchical_interleaved_bloom_filter
-
-int main()
+struct bin_data
 {
-    // Let's say we have groups that have data that we find interesting.
-    // For example, each file of the RefSeq data set could be such a group.
-    // In the context of the HIBF, we call such groups user bins.
+    std::vector<uint64_t> data{};
+};
+struct bins
+{
+    std::vector<bin_data> bins_data{};
+};
+bins my_bins{};
 
-    // Given a query, we want to quickly determine which user bins this query is likely to occur in.
-    // This is also called Approximate Membership Query (AMQ).
-
-    // In this example, we have three user bins. Each of these user bins is characterized by a range of
-    // unsigned integer values. Some popular techniques for obtaining such unsigned integers from
-    // biological sequences include k-mers, minimisers, and syncmers.
-
-    // For clarity, we show each user bin individually before copying them to user_bin_data.
-    std::vector<uint64_t> user_bin_1{1u, 2u, 3u, 4u, 5u, 6u, 7u, 8u, 9u, 10u};
-    std::vector<uint64_t> user_bin_2{1u, 2u, 3u, 4u, 5u};
-    std::vector<uint64_t> user_bin_3{3u, 9u, 11u};
-    std::vector<std::vector<uint64_t>> user_bin_data{user_bin_1, user_bin_2, user_bin_3};
-
-    // The HIBF uses a config. There are two required options:
-    // 1) The number of user bins: 3 (user_bin_data.size())
-    // 2) A function to access the input data.
-    //    The signature is (size_t const user_bin_id, seqan::hibf::insert_iterator it). You need to
-    //    provide the function body, and the hibf lib will use this function to access the data of each
-    //    user bin. When this function is called by the library with a specific user_bin_id, all
-    //    unsigned integer values (data) belonging to this user bin have to be assigned to the
-    //    seqan::hibf::insert_iterator.
-    //    Conveniently, this function can be a lambda, and hence capture data outside the function body.
-    auto get_user_bin_data = [&](size_t const user_bin_id, seqan::hibf::insert_iterator it)
+void with_config()
+{
+    auto input_lambda = [&](size_t const user_bin_id, seqan::hibf::insert_iterator it)
     {
-        for (auto value : user_bin_data[user_bin_id])
-            it = value;
+        for (auto const hash : my_bins.bins_data[user_bin_id].data)
+            it = hash;
     };
 
-    // Now we can construct a config, any other settings are optional. We have included some interesting
-    // settings with their respective default values here.
-    seqan::hibf::config config{.input_fn = get_user_bin_data, // required
-                               .number_of_user_bins = 3u,     // required
-                               .number_of_hash_functions = 2u,
+    seqan::hibf::config config{.input_fn = input_lambda,
+                               .number_of_user_bins = num_bins,
+                               .number_of_hash_functions = num_hashes,
                                .maximum_fpr = 0.05,
-                               .threads = 1u};
+                               .threads = num_threads};
 
-    // The HIBF constructor will determine a hierarchical layout for the user bins and build the filter.
-    seqan::hibf::hierarchical_interleaved_bloom_filter hibf{config};
+    // This will do I/O twice:
+    // 1. Determine the maximum number of elements in any bin. Used to compute the optimal bin size for the given FPR.
+    // 2. Insert all elements into the IBF.
+    seqan::hibf::interleaved_bloom_filter cpu_ibf{config};
+}
 
-    // Now we can search for some query.
-    std::vector<uint64_t> query1{3u, 9u, 12u, 14u};
+void without_config()
+{
+    // 8192u * 8192u might lead to a very high FPR, or might waste a lot of space.
+    seqan::hibf::interleaved_bloom_filter cpu_ibf{seqan::hibf::bin_count{num_bins},
+                                                  seqan::hibf::bin_size{8192u * 8192u},
+                                                  seqan::hibf::hash_function_count{num_hashes}};
 
-    // For this, we use the membership agent of the HIBF. This agent only needs to be created once and
-    // can be reused for multiple subsequent queries.
-    // If you are using multiple threads in your app, each thread should have its own membership agent.
-    auto agent = hibf.membership_agent();
+    // There will be concurrent write access for emplace.
+    // Giving each thread a chunk of bins to insert into the IBF will reduce the likelyhood of concurrent writes to the
+    // same memory. 64 bins per thread should be completely save, but 8 bins per thread allows for more parallelism.
+    // I've never seen problems with using 8 bins per thread. I have seen it without any chunking :)
+    size_t const chunk_size = std::clamp<size_t>(std::bit_ceil(num_bins / num_threads), 8u, 64u);
 
-    // The membership_for function takes the query and a threshold. Here, a threshold of two means that
-    // at least (>=) 2 values of the query must be found within a user bin to be a hit.
-    // While exact thresholds can be obtained for some approaches such as k-mers, another popular
-    // approach is to require at least x% of the values in the query to hit.
-    // For example, a threshold of 2 equals 40% of the values in query1 (5 values).
-    // This threshold needs to be provided by the user. In general, some care should be taken with the
-    // threshold. A low threshold requires a traversal of more parts of the hierarchy and slows down
-    // the search.
-    // Note that we bind the result with a `&` to avoid copies!
-    auto & result1 = agent.membership_for(query1, 2u);
+    // Parallelise over the input bins.
+    // For your setup, parallelising over the input data itself might be better.
+    // You could test both versions. Not sure what the overhead of the parallel for is.
+    // We usually do over the input bins because there might be some I/O - not the case for you.
+#pragma omp parallel for schedule(dynamic, chunk_size) num_threads(num_threads)
+    for (size_t user_bin_id = 0u; user_bin_id < num_bins; ++user_bin_id)
+    {
+        for (uint64_t const hash : my_bins.bins_data[user_bin_id].data)
+            cpu_ibf.emplace(hash, seqan::hibf::bin_index{user_bin_id});
+    }
+}
 
-    // query1 hits in user_bin_1 and user_bin_3, which have the IDs 0 and 2, respectively.
-    for (int64_t hit_user_bin : result1)
-        std::cout << hit_user_bin << ' '; // The results are not sorted: 2 0
-    std::cout << '\n';
+void query(seqan::hibf::interleaved_bloom_filter const & cpu_ibf, std::vector<uint64_t> const & queries)
+{
+    auto agent = cpu_ibf.membership_agent();
 
-    // Another query.
-    // A query is simply a range of unsigned integer values, e.g., it does not have to be a vector.
-    auto query2 = std::views::iota(0u, 15u); // 0,1,2,...,14
-    auto & result2 = agent.membership_for(query2, 5u);
-    agent.sort_results(); // Sort the results.
+// Each thread needs its own agent. The agent is not thread-safe.
+// The agent holds a bit_vector to store the results.
+// `bulk_contains` just returns a reference (!) to that vector.
+#pragma omp parallel for schedule(dynamic) num_threads(num_threads) private(agent)
+    for (size_t i = 0u; i < queries.size(); ++i)
+    {
+        auto & result = agent.bulk_contains(queries[i]);
+    }
 
-    // query2 hits in user_bin_1 and user_bin_2, which have the IDs 0 and 1, respectively.
-    for (int64_t hit_user_bin : result2)
-        std::cout << hit_user_bin << ' '; // The results are sorted: 0 1
-    std::cout << '\n';
+    // Above is the same as:
+    // #pragma omp parallel num_threads(num_threads)
+    //     {
+    //         auto agent = cpu_ibf.membership_agent();
+    // #pragma omp for schedule(dynamic)
+    //         for (size_t i = 0u; i < queries.size(); ++i)
+    //         {
+    //             auto & result = agent.bulk_contains(queries[i]);
+    //         }
+    //     }
 }
